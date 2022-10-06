78ca38044beac5006e40c05dcfe0f13578339534
Hooliper <111993000+Hooliper@users.noreply.github.com>
2022-10-06 16:31:12 -0300

    agregado el lector de excel

--- script.js
+++ script.js
@@ -81,7 +81,40 @@ $("#divTabla").hide();
 
 //LECTOR DE EXCEL
 
-
+$('#formFile').on('change', handleFileLoad = (e) => {
+    console.log("dentro");
+    if (e.target.files.length > 0) {
+        try {
+            this.setState({ operationInProgress: true, fileLoadProgress: 0 });
+            let file = e.target.files[0];
+            let reader = new FileReader();
+    
+            reader.onload = async function (e) {
+                let data = new Uint8Array(e.target.result);
+                let workbook = XLSX.read(data, { type: "array" });
+                let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+                let sheet = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+    
+                let dataset = await this.parseFileContent(sheet);
+                this.setState({ 
+                    dataset: dataset,
+                    fileLoaded: true,
+                    fileName: file.name,
+                    operationInProgress: false
+                });
+            }.bind(this);
+            reader.readAsArrayBuffer(file);
+        } catch (exception) {
+            this.setState({
+                fileLoaded: false,
+                fileName: "",
+                operationInProgress: false
+            });
+        }
+    } else {
+        toast("No files found", { type: "error" });
+    }
+});
 
 
 //FIN LECTOR EXCEL
