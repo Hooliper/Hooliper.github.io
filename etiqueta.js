bde0ac54d000e2c7c951af6891425d851538a579
Hooliper <111993000+Hooliper@users.noreply.github.com>
2023-04-21 19:29:51 -0300

    Validacion input codigo y precios

---
+++ etiqueta.js
@@ -0,0 +1,12 @@
+let fecha = new Date().toLocaleString();
+$( ".part12" ).html( fecha );
+
+console.log(localStorage["bar"])
+
+/*
+window.print();
+
+window.addEventListener("afterprint", function() {
+    window.close();
+});
+*/
--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const planes = [
 function listaPlanes(idSelect, planElegido) {
     for (let i = 0; i < planes.length; i++){
         if (planElegido == i){
-             $('#'+idSelect).append('<option value='+i+' selected>'+planes[i][0]+'</option>');
+            $('#'+idSelect).append('<option value='+i+' selected>'+planes[i][0]+'</option>');
         } else {
-             $('#'+idSelect).append('<option value='+i+'>'+planes[i][0]+'</option>');
+            $('#'+idSelect).append('<option value='+i+'>'+planes[i][0]+'</option>');
         } 
     } 
 }
@@ -21,7 +21,7 @@ let numeroFila = 1;
 
 function agregaFilaManual(){
     let cod = $('#codigo').val();
-    let descripcion = $('#descripcion').html();
+    let descripcion = $('#descripcion').val();
     let precio = $('#precio').val();
     let cr = $('#cr').val();
     let totalPrecioCr = $('#total').html();
@@ -29,17 +29,17 @@ function agregaFilaManual(){
     let cant = $('#cant').val();
     
     $('#codigo').val('');
-    $('#descripcion').html('');
+    $('#descripcion').val('');
     $('#precio').val('');
     $('#cr').val('');
-    $('#total').html('');
+    $('#total').html(0);
     $('#cant').val(1);
 
     agregaFila(cod, descripcion, precio, cr, totalPrecioCr, planElegido, cant);
 }
 
 function agregaFilaExcel(arrExcel){
-    console.log("2");
+    $("#divTabla").show(400);
     for (let i = 0; i < arrExcel.length; i++) {
         let cod = arrExcel[i]["Articulo"];
         let descripcion = arrExcel[i]["Desc. Articulo"];
@@ -76,25 +76,92 @@ function agregaFila(cod, descripcion, precio, cr, totalPrecioCr, planElegido, ca
 
 $('.enterNuevaFila').on('keypress',function(e) {
     if(e.which == 13) {
-        $("#divTabla").show(400);
-        agregaFilaManual();
+        let codigo = $('#codigo').val();
+        if (localStorage[codigo]){
+            let articulo = JSON.parse(localStorage[codigo]);
+            $('#descripcion').val(articulo["desc"]);
+            $("#divTabla").show(400);
+            agregaFilaManual();
+        }
+        else{
+            errorDeCodigo();
+        }
+    }
+});
+
+$('#codigo').on('focusout', function(){
+    let codigo = $('#codigo').val();
+    if (codigo.length > 0){
+        if (localStorage[codigo]){
+            let articulo = JSON.parse(localStorage[codigo]);
+            $('#descripcion').val(articulo["desc"]);
+        }
+        else{
+            errorDeCodigo();
+        }
+    }
+});
+
+function errorDeCodigo(){
+    let err = '<label class="codError">Código erroneo <br>o inexistente</label>';
+    $('#codLabel').append(err);
+    setTimeout(() => {
+        $(".codError").fadeOut(500);
+      }, 2500);
+}
+
+//validacion inputs solo numeros: Codigo, precio, CR, Cant
+document.querySelector(".soloNumero").addEventListener("keypress", function (evt) {
+    if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
+    {
+        evt.preventDefault();
+    }
+});
+document.querySelector("#precio").addEventListener("keypress", function (evt) {
+    let a = $("#precio").val();
+    let punto = a.indexOf(".");    
+    if (a.length < 1 || punto > -1){
+            if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
+        {
+            evt.preventDefault();
+        }
+    } else {
+        if (evt.which != 8 && evt.which != 0 && evt.which != 46 && evt.which < 48 || evt.which > 57)
+        {
+            evt.preventDefault();
+        }
+    }
+});
+document.querySelector("#cr").addEventListener("keypress", function (evt) {
+    let a = $("#cr").val();
+    let punto = a.indexOf(".");    
+    if (a.length < 1 || punto > -1){
+            if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
+        {
+            evt.preventDefault();
+        }
+    } else {
+        if (evt.which != 8 && evt.which != 0 && evt.which != 46 && evt.which < 48 || evt.which > 57)
+        {
+            evt.preventDefault();
+        }
     }
 });
 
 $('#precio').on('input', (sumaPrecioCr));
-    
 $('#cr').on('input', (sumaPrecioCr));
-    
 function sumaPrecioCr(){
     let a = Number($('#precio').val());
     let b = Number($('#cr').val());
-    
-    $('#total').html(a+b);
+    $('#total').html((a+b).toFixed(2));
 }
     
 //Oculta la lista de articulos cargados
 $("#divTabla").hide();
 
+function imprimir(){
+    window.open("etiqueta.html","miventana","width=600,height=600,menubar=no")
+}
 
 
 //LECTOR DE EXCEL
@@ -116,16 +183,45 @@ function handleFiles(files){
             result.push(...newData)
         }
       }
-      console.log("1");
       agregaFilaExcel(result);
-      console.log(result);
-      //console.log(zzexcel.SheetNames)
-      //console.log(result[5]["Desc. Articulo"])
+      //console.log(result);
+      //console.log(zzexcel.SheetNames);
+      //console.log(result[5]["Desc. Articulo"]);
     }
 };
 
+function actualizaBd(files){
+    let file = files[0];
+
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = (e) => {
+      const data = e.target.result;
+      const zzexcel = window.XLSX.read(data, {
+        type: 'binary'
+      });
+      const res = [];
+      for (let i = 0; i < zzexcel.SheetNames.length; i++) {
+        if (zzexcel.SheetNames[i] == "BD"){
+            const newData = window.XLSX.utils.sheet_to_json(zzexcel.Sheets[zzexcel.SheetNames[i]]);
+            res.push(...newData)
+        }
+      }
+
+      //Agrega los productos del excel a localStorage usando codigo como key
+      for (let i = 0; i < res.length; i++){
+        let articulo = JSON.stringify(res[i]);
+        localStorage[res[i]["cod"]] = articulo;
+      }
+      alert("base de datos actualizada");
+    }
+};
 //FIN LECTOR EXCEL
 
-
-
-
+//Local Storage
+//localStorage["bar"] = "LG TV";
+//console.log(localStorage["bar"]);
+ /*
+localStorage.setItem('videoDetails', JSON.stringify(videoDetails) );
+var videoDetails = JSON.parse(localStorage.getItem('videoDetails');
+*/
