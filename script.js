e0bcd26356eb8d2d929103c762bf386e27b868cb
Hooliper <111993000+Hooliper@users.noreply.github.com>
2023-04-27 14:38:20 -0300

    update 27-04

--- script.js
+++ script.js
@@ -41,47 +41,54 @@ function agregaFilaManual(){
 function agregaFilaExcel(arrExcel){
     $("#divTabla").show(400);
     for (let i = 0; i < arrExcel.length; i++) {
-        let cod = arrExcel[i]["Articulo"];
-        let descripcion = arrExcel[i]["Desc. Articulo"];
-        let precio = arrExcel[i]["Precio Vigente"];
-        let cr = 0;
-        let totalPrecioCr = precio;
-        let planElegido = 0;
-        let cant = 1;
-        agregaFila(cod, descripcion, precio, cr, totalPrecioCr, planElegido, cant);
+        if (arrExcel[i]["Articulo"] !== "Total") {
+            let cod = arrExcel[i]["Articulo"];
+            let descripcion = arrExcel[i]["Desc. Articulo"];
+            let precio = arrExcel[i]["Precio Vigente"].toFixed(2);
+            let cr = 0;
+            let totalPrecioCr = precio;
+            let planElegido = 0;
+            let cant = 1;
+            agregaFila(cod, descripcion, precio, cr, totalPrecioCr, planElegido, cant);
+        }
     }
 }
 
-function agregaFila(cod, descripcion, precio, cr, totalPrecioCr, planElegido, cant){    
-    let fila = '<tr>'+
-                '<td class="celdaAlineadaDerecha">'+numeroFila+'</td>'+
+function agregaFila(cod, descripcion, precio, cr, totalPrecioCr, planElegido, cant){
+    let fila = '<tr id="'+numeroFila+'">'+
+                '<td>'+numeroFila+'</td>'+
                 '<td class="celdaAlineadaDerecha">'+cod+'</td>'+
                 '<td class="celdaDesc">'+descripcion+'</td>'+
-                '<td class="celdaAlineadaDerecha">'+precio+'</td>'+
-                '<td class="celdaAlineadaDerecha">'+cr+'</td>'+
-                '<td class="celdaAlineadaDerecha">'+totalPrecioCr+'</td>'+
+                '<td><input type="text" value="'+precio+'" class="celdaPrecio inputPrecio form-control" readonly tabindex="-1"></td>'+
+                '<td><input type="text" value="'+cr+'" class="celdaCr inputPrecio form-control" oninput="sum(this)" maxlength="9" onkeypress="validaSoloNumero(event, value)"></input></td>'+
+                '<td><input type="text" value="'+totalPrecioCr+'" class="celdaTotalPrecioCr inputPrecio form-control" readonly tabindex="-1"></td>'+
                 '<td>'+
                     '<select id="planFila'+numeroFila+'" class="inputSelect form-control">'+
                         
                     '</select>'+
-                '<td><input type="number" min="0" max="12" class="inputCant form-control" value="'+cant+'"></td>'+  
+                '<td><input type="number" min="0" max="8" class="inputCant form-control" value="'+cant+'" onkeypress="soloNumeroCant(event, value)" oninput="limitMax(this)"></td>'+  
                 '</td>'+
+                '<td><b><a href="#" class="borrar" onclick="eliminaFila(this)">X</a></b><td/>'+
             '</tr>';
 
     $('#tabla tbody').append(fila); 
     listaPlanes("planFila"+numeroFila, planElegido)
-    numeroFila += 1;
-           
+    numeroFila += 1;     
 }
 
 $('.enterNuevaFila').on('keypress',function(e) {
     if(e.which == 13) {
         let codigo = $('#codigo').val();
         if (localStorage[codigo]){
-            let articulo = JSON.parse(localStorage[codigo]);
-            $('#descripcion').val(articulo["desc"]);
-            $("#divTabla").show(400);
-            agregaFilaManual();
+            if ($('#precio').val() > 0) {
+                let articulo = JSON.parse(localStorage[codigo]);
+                $('#descripcion').val(articulo["desc"]);
+                $("#divTabla").show(400);
+                agregaFilaManual(); 
+            }
+            else{
+                errorDePrecio();
+            }
         }
         else{
             errorDeCodigo();
@@ -89,6 +96,7 @@ $('.enterNuevaFila').on('keypress',function(e) {
     }
 });
 
+//busca descripcion al salir del input cod
 $('#codigo').on('focusout', function(){
     let codigo = $('#codigo').val();
     if (codigo.length > 0){
@@ -110,44 +118,61 @@ function errorDeCodigo(){
       }, 2500);
 }
 
-//validacion inputs solo numeros: Codigo, precio, CR, Cant
+function errorDePrecio(){
+    let err = '<label class="codError">Debe ingresar el<br>precio del artículo</label>';
+    $('#precioLabel').append(err);
+    setTimeout(() => {
+        $(".codError").fadeOut(500);
+      }, 2500);
+}
+
+//validacion solo numeros: Codigo
 document.querySelector(".soloNumero").addEventListener("keypress", function (evt) {
     if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
     {
         evt.preventDefault();
     }
 });
-document.querySelector("#precio").addEventListener("keypress", function (evt) {
-    let a = $("#precio").val();
-    let punto = a.indexOf(".");    
-    if (a.length < 1 || punto > -1){
-            if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
-        {
-            evt.preventDefault();
-        }
-    } else {
-        if (evt.which != 8 && evt.which != 0 && evt.which != 46 && evt.which < 48 || evt.which > 57)
-        {
-            evt.preventDefault();
-        }
-    }
-});
-document.querySelector("#cr").addEventListener("keypress", function (evt) {
-    let a = $("#cr").val();
-    let punto = a.indexOf(".");    
-    if (a.length < 1 || punto > -1){
-            if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
-        {
-            evt.preventDefault();
-        }
-    } else {
-        if (evt.which != 8 && evt.which != 0 && evt.which != 46 && evt.which < 48 || evt.which > 57)
-        {
-            evt.preventDefault();
-        }
-    }
-});
 
+//validacion solo numeros con decimales: Precios y CR 
+function validaSoloNumero(evt, val){
+    let a = val;
+    let punto = a.indexOf(".");
+        if (a.length < 1 || punto > -1){
+            if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
+            {
+                evt.preventDefault();
+            }
+        } else {
+        if (evt.which != 8 && evt.which != 0 && evt.which != 46 && evt.which < 48 || evt.which > 57)
+            {
+                evt.preventDefault();
+            }
+        }
+}
+
+function soloNumeroCant(evt, val){
+    if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
+        {
+            evt.preventDefault();
+        }
+}
+
+//limita maxima cantidad de etiquetas a 8
+function limitMax(a){
+    let idFila = $(a).parents('tr').attr('id');
+    let val = Number($('#'+idFila+' .inputCant').val());
+    if (val > 8) {
+        $('#'+idFila+' .inputCant').val(8);
+    }
+}
+
+function eliminaFila(a) {
+    let idFila = $(a).parents('tr').attr('id');
+    $('#'+idFila).remove();
+}
+
+//suma precio + cr en columna de carga
 $('#precio').on('input', (sumaPrecioCr));
 $('#cr').on('input', (sumaPrecioCr));
 function sumaPrecioCr(){
@@ -155,7 +180,15 @@ function sumaPrecioCr(){
     let b = Number($('#cr').val());
     $('#total').html((a+b).toFixed(2));
 }
-    
+
+//suma precio + cr en lista cargada
+function sum(a){
+    let idFila = $(a).parents('tr').attr('id');
+    let celdaPrecio = Number($('#'+idFila+' .celdaPrecio').val());
+    let celdaCr = Number($('#'+idFila+' .celdaCr').val());
+    $('#'+idFila+' .celdaTotalPrecioCr').val((celdaPrecio+celdaCr).toFixed(2));
+}
+
 //Oculta la lista de articulos cargados
 $("#divTabla").hide();
 
